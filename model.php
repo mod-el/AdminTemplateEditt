3336c123894539fa9341ac7805729726b189382a
Domenico <sviluppo@editt.it>
2017-09-19 14:36:23 +0200

    Too many spaces :)

--- model.php
+++ model.php
@@ -4,11 +4,11 @@ $moduleData = [
 	'description' => 'Admin template "Editt"',
 	'version' => '0.1.0',
 	'dependencies' => [
-		'Admin'  =>  '>=0.14.0',
-		'Output'  =>  '>=0.1.0',
-		'FrontEnd'  =>  '>=1.1.0',
-		'ResizeTable'  =>  '>=1.2.0',
-		'Popup'  =>  '>=1.0.0',
+		'Admin' => '>=0.14.0',
+		'Output' => '>=0.1.0',
+		'FrontEnd' => '>=1.1.0',
+		'ResizeTable' => '>=1.2.0',
+		'Popup' => '>=1.0.0',
 	],
 	'is-admin-template' => true,
 	'css' => [
