b94c7772ae168a98cf5bb9f2141841b3bb1fb871
Domenico Giambra <sviluppo@editt.it>
2018-01-17 17:10:44 +0100

    CSRF token cannot be cached by the Service Worker

--- Controllers/AdminServiceWorkerController.php
+++ Controllers/AdminServiceWorkerController.php
@@ -8,7 +8,7 @@ class AdminServiceWorkerController extends Controller {
 	}
 
 	public function index(){
-		$assets = $this->model->_AdminTemplateEditt->getAssets();
+		$assets = $this->model->_AdminTemplateEditt->getAssetsForServiceWorker();
 		require(INCLUDE_PATH.'model'.DIRECTORY_SEPARATOR.'AdminTemplateEditt'.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'cache-key.php');
 		require(INCLUDE_PATH.'model'.DIRECTORY_SEPARATOR.'AdminTemplateEditt'.DIRECTORY_SEPARATOR.'files'.DIRECTORY_SEPARATOR.'sw.js');
 		die();
