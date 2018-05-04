8d747a7af316e1977bfbf6e5bd1f61626aa1a444
Domenico Giambra <sviluppo@editt.it>
2018-05-04 13:18:52 +0200

    Workaround for new invasive Chrome autofill feature

--- templates/form-header.php
+++ templates/form-header.php
@@ -19,8 +19,8 @@ if(!isset($_GET['print'])){
 
 <form action="" method="post" id="adminForm" name="adminForm" onsubmit="save(); return false" data-filled="0">
 	<!-- fake fields are a workaround for chrome autofill getting the wrong fields -->
-	<input style="display:none" type="text" name="fakeusernameremembered" />
-	<input style="display:none" type="password" name="fakepasswordremembered" />
+	<input style="position:absolute;top:-1000px" type="text" name="fakeusernameremembered" />
+	<input style="position:absolute;top:-1000px" type="password" name="fakepasswordremembered" />
 
     <input type="hidden" name="_model_version" value="1" />
     <input type="hidden" name="_mandatory_fields" value="<?=entities(json_encode($this->model->_Admin->options['required']))?>" />
