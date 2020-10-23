c88f5fb4ac22a734fcdb97258dd6b11a037a7fd4
Domenico Giambra <sviluppo@editt.it>
2020-10-23 16:17:14 +0200

    Refactor form e history

--- templates/page-header.php
+++ templates/page-header.php
@@ -3,8 +3,8 @@ if (!isset($_GET['print'])) {
 	?>
 	<div id="history-box" style="right: -15%">
 		<div class="text-center pad10v">
-			[<a href="#" onclick="historyStepBack(); return false"> indietro </a>]
-			[<a href="#" onclick="historyStepForward(); return false"> avanti </a>]
+			[<a href="#" onclick="historyMgr.stepBack(); return false"> indietro </a>]
+			[<a href="#" onclick="historyMgr.stepForward(); return false"> avanti </a>]
 			[<a href="#" onclick="switchHistoryBox(); return false"> chiudi </a>]
 		</div>
 		<div id="links-history"></div>
