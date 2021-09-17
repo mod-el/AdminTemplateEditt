dc3073e6053d1d79cc60e832a3f306b2e4e67c39
Domenico Giambra <zorinik@gmail.com>
2021-09-17 11:06:09 +0200

    Config per navigazione cronologia

--- templates/shell.php
+++ templates/shell.php
@@ -15,6 +15,7 @@ $this->languageBound = true;
 		var adminApiPath = '<?=$this->model->_Admin->getApiPath()?>';
 		var model_notifications_user_idx = 'Admin';
 		var model_notifications_user = null;
+		var enableHistoryNavigation = <?=json_encode($adminFrontConfig['enableHistoryNavigation'])?>;
 	</script>
 
 	<style>
